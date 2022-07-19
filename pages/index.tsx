721e6ebb0fe5760089f7b3f769a64e8e9a436c3d
kimm950 <kimuo950@gmail.com>
2022-07-19 19:40:43 +0900

    add title

--- pages/index.tsx
+++ pages/index.tsx
@@ -45,7 +45,7 @@ const Home: NextPage<Props> = ({ posts }) => {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
-        <title>My blog</title>
+        <title>Kimm's Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
